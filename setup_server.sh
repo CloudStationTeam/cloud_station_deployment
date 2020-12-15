ac93333746e295bf473810afc9ae58cb8e761962
Chris Zhang <chizhang95@gmail.com>
2020-12-14 16:34:18 -0800

    edit checkout process

--- setup_server.sh
+++ setup_server.sh
@@ -5,9 +5,9 @@ sudo apt-get --yes update
 sudo apt-get --yes upgrade
 
 echo "2. Installing NGINX and docker"
-sudo apt-get install -y nginx
-sudo apt-get remove -y docker docker-engine docker.io
-sudo apt install -y docker.io
+sudo apt-get --yes install nginx
+sudo apt-get --yes remove docker docker-engine docker.io
+sudo apt --yes install docker.io
 
 echo "3. Cloning CloudStation web app source code"
 git clone https://github.com/CloudStationTeam/cloud_station_web.git ~/cloud_station_web
@@ -19,7 +19,9 @@ do
         --tag=*)
 		TAG="${arg#*=}"
 		echo "Checkout out release $TAG"
+		cd ~/cloud_station_web
         git checkout $TAG
+        cd ~
         shift # Remove --cache= from processing
         ;;
         *)
@@ -29,12 +31,12 @@ do
 done
 
 echo "4. Setting up Python virtual environment"
-sudo apt-get install -y python3-venv
+sudo apt-get --yes install python3-venv
 mkdir ~/ENV
 python3 -m venv ~/ENV
 source ~/ENV/bin/activate
 # These dependencies are required for pymavlink
-sudo apt-get install -y libxml2-dev libxslt-dev python3-dev
+sudo apt-get --yes install libxml2-dev libxslt-dev python3-dev
 pip3 install -r ~/cloud_station_web/requirements.txt --no-cache-dir
 # git config --global core.editor "vim"
 echo "Finished setting up server!"
