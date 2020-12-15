acd9fc8eb40dfa3164688d4bc2b8e907134fbf6b
Chris Zhang <chizhang95@gmail.com>
2020-12-14 16:19:28 -0800

    add -y flag to apt-get, added flag for release tags

--- setup_server.sh
+++ setup_server.sh
@@ -12,13 +12,29 @@ sudo apt install docker.io
 echo "3. Cloning CloudStation web app source code"
 git clone https://github.com/CloudStationTeam/cloud_station_web.git ~/cloud_station_web
 
+# Checkout release if specified
+for arg in "$@"
+do
+    case $arg in
+        --tag=*)
+		TAG="${arg#*=}"
+		echo "Checkout out release $TAG"
+        git checkout $TAG
+        shift # Remove --cache= from processing
+        ;;
+        *)
+        shift # Remove generic argument from processing
+        ;;
+    esac
+done
+
 echo "4. Setting up Python virtual environment"
-sudo apt-get install python3-venv
+sudo apt-get install -y python3-venv
 mkdir ~/ENV
 python3 -m venv ~/ENV
 source ~/ENV/bin/activate
 # These dependencies are required for pymavlink
-sudo apt-get install libxml2-dev libxslt-dev python3-dev
+sudo apt-get install -y libxml2-dev libxslt-dev python3-dev
 pip3 install -r ~/cloud_station_web/requirements.txt --no-cache-dir
 # git config --global core.editor "vim"
 echo "Finished setting up server!"
