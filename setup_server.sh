17ef59711016c1ac07c8c63e4c9d64b4bfe17c11
Chris Zhang <chizhang95@gmail.com>
2020-12-14 16:42:07 -0800

    Deploy script works with tagged releases (#2)
    Users can specify which release to use with --tag=
    If no tag, pulls latest commit by default
    
    Also adds -y flags to all installations to remove prompts

--- setup_server.sh
+++ setup_server.sh
@@ -1,24 +1,42 @@
 #!/bin/bash
 echo "######### Setting up server #########"
 echo "1. Updating Ubuntu"
-sudo apt-get update
-sudo apt-get upgrade
+sudo apt-get --yes update
+sudo apt-get --yes upgrade
 
 echo "2. Installing NGINX and docker"
-sudo apt-get install nginx
-sudo apt-get remove docker docker-engine docker.io
-sudo apt install docker.io
+sudo apt-get --yes install nginx
+sudo apt-get --yes remove docker docker-engine docker.io
+sudo apt --yes install docker.io
 
 echo "3. Cloning CloudStation web app source code"
 git clone https://github.com/CloudStationTeam/cloud_station_web.git ~/cloud_station_web
 
+# Checkout release if specified
+for arg in "$@"
+do
+    case $arg in
+        --tag=*)
+		TAG="${arg#*=}"
+		echo "Checkout out release $TAG"
+		cd ~/cloud_station_web
+        git checkout $TAG
+        cd ~
+        shift # Remove --cache= from processing
+        ;;
+        *)
+        shift # Remove generic argument from processing
+        ;;
+    esac
+done
+
 echo "4. Setting up Python virtual environment"
-sudo apt-get install python3-venv
+sudo apt-get --yes install python3-venv
 mkdir ~/ENV
 python3 -m venv ~/ENV
 source ~/ENV/bin/activate
 # These dependencies are required for pymavlink
-sudo apt-get install libxml2-dev libxslt-dev python3-dev
+sudo apt-get --yes install libxml2-dev libxslt-dev python3-dev
 pip3 install -r ~/cloud_station_web/requirements.txt --no-cache-dir
 # git config --global core.editor "vim"
 echo "Finished setting up server!"
