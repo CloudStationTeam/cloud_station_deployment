8c5131b4ed0017478061e62827aba08b348f8503
haixu-qin <69027122+haixu-qin@users.noreply.github.com>
2023-11-19 19:54:17 -0800

    Create rm_env.py

---
+++ rm_env.py
@@ -0,0 +1,38 @@
+#!/bin/bash
+rm -r ENV
+
+rm -rf cloud_station_deployment 
+
+rm -rf cloud_station_web
+
+git clone https://github.com/CloudStationTeam/cloud_station_deployment.git
+sleep 3
+
+bash ~/cloud_station_deployment/setup_server.sh
+
+# Refer to 
+# https://cloud-station-docs.readthedocs.io/en/latest/deployment.html#step-by-step-deployment-guide 
+# to edit the files.
+
+#ps aux | grep nginx | grep -v grep #find nginx user
+#sudo nginx -T | grep "cache" #check for cache files
+
+sudo usermod -a -G ubuntu www-data #add nginx user to the ubuntu group 
+# optional 
+sudo chgrp -R ubuntu * #change file owners to ubuntu group 
+sudo chmod -R g+r * #change file permissions of file owners 
+
+
+bash ~/cloud_station_deployment/configure_web_server.sh 
+
+
+bash ~/cloud_station_deployment/reload_server.sh 
+
+
+echo "Print Logs"
+journalctl -u daphne.service | tail #print logs 
+
+
+source ./ENV/bin/activate  # Activate the Env 
+pip freeze  # List installed packages
+deactivate
