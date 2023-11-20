d4e979a2c9c5c011c8872237b08f92d0356506e0
haixu-qin <69027122+haixu-qin@users.noreply.github.com>
2023-11-19 21:40:03 -0800

    Update rmv_env.sh

--- rmv_env.sh
+++ rmv_env.sh
@@ -8,6 +8,7 @@ rm -rf cloud_station_web
 git clone https://github.com/CloudStationTeam/cloud_station_deployment.git
 sleep 3
 
+# Edit setup_server.sh if your repo is a different one.
 bash ~/cloud_station_deployment/setup_server.sh
 
 # Refer to 
@@ -22,12 +23,11 @@ sudo usermod -a -G ubuntu www-data #add nginx user to the ubuntu group
 sudo chgrp -R ubuntu * #change file owners to ubuntu group 
 sudo chmod -R g+r * #change file permissions of file owners 
 
-
 bash ~/cloud_station_deployment/configure_web_server.sh 
 
 
+#Edit reload_server.sh if your repo is a different one.
 bash ~/cloud_station_deployment/reload_server.sh 
-
 
 echo "Print Logs"
 journalctl -u daphne.service | tail #print logs 
