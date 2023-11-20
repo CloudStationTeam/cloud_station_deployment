dc924505d91c441bf3cb917dc073758a6f531fef
haixu-qin <69027122+haixu-qin@users.noreply.github.com>
2023-11-19 21:40:28 -0800

    Update rmv_env.sh

--- rmv_env.sh
+++ rmv_env.sh
@@ -26,7 +26,7 @@ sudo chmod -R g+r * #change file permissions of file owners
 bash ~/cloud_station_deployment/configure_web_server.sh 
 
 
-#Edit reload_server.sh if your repo is a different one.
+# Edit reload_server.sh if your repo is a different one.
 bash ~/cloud_station_deployment/reload_server.sh 
 
 echo "Print Logs"
